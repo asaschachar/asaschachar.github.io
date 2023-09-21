87364ff5ed10759630658bb7913d89bf175f3e78
Asa Schachar <asa.schachar+gh@gmail.com>
2023-09-20 21:18:29 -0700

    Experiment with AWS image storage

--- fb-marketplace/data.js
+++ fb-marketplace/data.js
@@ -1,181 +1,198 @@
+const URL_PATH = "https://asa-playground-public.s3.amazonaws.com";
+
 var DEFAULT_ITEMS = [
   {
     id: 1,
     name: "Eras Tour Tickets",
     price: "$1500",
-    location: "Ships to You",
-    images: [
-      "./images/ErasTour.webp",
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    location: "At Albert & Neena's",
+    images: [`${URL_PATH}/ErasTour.webp`, `${URL_PATH}/ErasTour.webp`],
   },
   {
     id: 2,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Barbie Dolls",
+    price: "$30",
+    location: "Ships to you",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 3,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Spa Day",
+    price: "$175",
+    location: "With spa dog",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 4,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Cake",
+    price: "$30",
+    location: "At neighbor's place",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 5,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Sounder's Tickets",
+    price: "$185",
+    location: "In the mail",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 6,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Trampoline",
+    price: "$35",
+    location: "Down the street",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 7,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Alpine Lake Hike",
+    price: "$18",
+    location: "On a Mountain",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 8,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Paint Job",
+    price: "$75",
+    location: "Comes to You",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 9,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Motel Stay",
+    price: "$145",
+    location: "On Aurora",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 10,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Four-legged friend",
+    price: "$500",
+    location: "Comes on Command",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 11,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Love Lock",
+    price: "Priceless",
+    location: "On a pier in SF",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 12,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Tulips",
+    price: "$65",
+    location: "In your Yard",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 13,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Meditation Retreat",
+    price: "$1200",
+    location: "Somewhere quiet",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 14,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Fancy Dinner",
+    price: "$115",
+    location: "Where Eli is",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 15,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Kayak",
+    price: "$215",
+    location: "On the water",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 16,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Ice Cream",
+    price: "$12",
+    location: "Within Walking Distance",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 17,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "REI Date",
+    price: "$25",
+    location: "Just down the road",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 18,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Home Cooked Meal",
+    price: "$55",
+    location: "In Fridge",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 19,
-    name: "Lambourghini",
-    price: "$45",
-    location: "Ships to You",
+    name: "Open House",
+    price: "$5",
+    location: "In Your Favorite Neighborhood",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
   },
   {
     id: 20,
-    name: "Lambourghini",
+    name: "Notarization",
     price: "$45",
-    location: "Ships to You",
+    location: "UPS Store",
+    images: [
+      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+    ],
+  },
+  {
+    id: 21,
+    name: "Yoga Class",
+    price: "$32",
+    location: "On the beach",
+    images: [
+      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+    ],
+  },
+  {
+    id: 22,
+    name: "Sailing Trip",
+    price: "$3200",
+    location: "Between Islands",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
@@ -185,9 +202,9 @@ var DEFAULT_ITEMS = [
 var STEP1 = [
   {
     id: 9.5,
-    name: "Ferrari",
+    name: "Non-alcoholic Beer",
     price: "Free!",
-    location: "Have to locate!",
+    location: "Somewhere in the house",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
@@ -197,9 +214,21 @@ var STEP1 = [
 var STEP2 = [
   {
     id: 16.5,
-    name: "Ferrari",
+    name: "Pickleball with Family",
     price: "Free!",
-    location: "Have to locate!",
+    location: "Parent's House",
+    images: [
+      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+    ],
+  },
+];
+
+var STEP3 = [
+  {
+    id: 4.5,
+    name: "DoorDash",
+    price: "Free!",
+    location: "Somewhere outside the house",
     images: [
       "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
     ],
@@ -209,6 +238,7 @@ var STEP2 = [
 var MARKETPLACE = {
   0: [...STEP1, ...DEFAULT_ITEMS],
   1: [...STEP2, ...DEFAULT_ITEMS],
+  2: [...STEP3, ...DEFAULT_ITEMS],
 };
 
 var ALL_ITEMS = [...STEP1, ...STEP2, ...DEFAULT_ITEMS];
