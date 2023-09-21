70839d1951799b44d3a9dbe30441dea2a4e2a1e6
Asa Schachar <asa.schachar+gh@gmail.com>
2023-09-20 23:05:47 -0700

    add images

--- fb-marketplace/data.js
+++ fb-marketplace/data.js
@@ -6,7 +6,7 @@ var DEFAULT_ITEMS = [
     name: "Eras Tour Tickets",
     price: "$1500",
     location: "At Albert & Neena's",
-    images: [`${URL_PATH}/ErasTour.webp`, `${URL_PATH}/ErasTour.webp`],
+    images: [`${URL_PATH}/ErasTour.webp`, `${URL_PATH}/cc-swift.jpg`],
   },
   {
     id: 2,
@@ -14,7 +14,10 @@ var DEFAULT_ITEMS = [
     price: "$30",
     location: "Ships to you",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/barbie.jpeg`,
+      `${URL_PATH}/doll1.jpeg`,
+      `${URL_PATH}/doll2.jpeg`,
+      `${URL_PATH}/doll3.jpeg`,
     ],
   },
   {
@@ -23,7 +26,9 @@ var DEFAULT_ITEMS = [
     price: "$175",
     location: "With spa dog",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/mk-spa-day.jpeg`,
+      `${URL_PATH}/spa-day.jpeg`,
+      `${URL_PATH}/water.jpeg`,
     ],
   },
   {
@@ -31,27 +36,21 @@ var DEFAULT_ITEMS = [
     name: "Cake",
     price: "$30",
     location: "At neighbor's place",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-cake.jpeg`, `${URL_PATH}/cake.jpeg`],
   },
   {
     id: 5,
     name: "Sounder's Tickets",
     price: "$185",
     location: "In the mail",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-sounders.jpeg`, `${URL_PATH}/soccer-tickets.png`],
   },
   {
     id: 6,
     name: "Trampoline",
     price: "$35",
     location: "Down the street",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-trampoline.jpeg`, `${URL_PATH}/trampoline.jpeg`],
   },
   {
     id: 7,
@@ -59,7 +58,9 @@ var DEFAULT_ITEMS = [
     price: "$18",
     location: "On a Mountain",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/mk-alpine-lake.jpeg`,
+      `${URL_PATH}/hike.jpeg`,
+      `${URL_PATH}/hike-2.jpeg`,
     ],
   },
   {
@@ -67,9 +68,7 @@ var DEFAULT_ITEMS = [
     name: "Paint Job",
     price: "$75",
     location: "Comes to You",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-paint.jpeg`, `${URL_PATH}/paint-job.jpeg`],
   },
   {
     id: 9,
@@ -77,7 +76,10 @@ var DEFAULT_ITEMS = [
     price: "$145",
     location: "On Aurora",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/mk-motel.jpeg`,
+      `${URL_PATH}/motel-job.jpeg`,
+      `${URL_PATH}/motel.jpeg`,
+      `${URL_PATH}/laundry.png`,
     ],
   },
   {
@@ -86,7 +88,10 @@ var DEFAULT_ITEMS = [
     price: "$500",
     location: "Comes on Command",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/mk-dog.jpeg`,
+      `${URL_PATH}/cc-dog.jpg`,
+      `${URL_PATH}/c-and-bo.jpeg`,
+      `${URL_PATH}/c-and-bo2.jpeg`,
     ],
   },
   {
@@ -94,18 +99,14 @@ var DEFAULT_ITEMS = [
     name: "Love Lock",
     price: "Priceless",
     location: "On a pier in SF",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-love-lock.jpeg`, `${URL_PATH}/lock.jpeg`],
   },
   {
     id: 12,
     name: "Tulips",
     price: "$65",
     location: "In your Yard",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-tulips.jpeg`, `${URL_PATH}/flowers.jpeg`],
   },
   {
     id: 13,
@@ -113,7 +114,9 @@ var DEFAULT_ITEMS = [
     price: "$1200",
     location: "Somewhere quiet",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/mk-meditation.jpeg`,
+      `${URL_PATH}/meditation-retreat.jpeg`,
+      `${URL_PATH}/bed.jpeg`,
     ],
   },
   {
@@ -122,7 +125,14 @@ var DEFAULT_ITEMS = [
     price: "$115",
     location: "Where Eli is",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/mk-fancy-meal.jpeg`,
+      `${URL_PATH}/nice-meal.jpeg`,
+      `${URL_PATH}/nice-meal2.jpeg`,
+      `${URL_PATH}/pasta.jpeg`,
+      `${URL_PATH}/bun.jpeg`,
+      `${URL_PATH}/asian-dessert.jpeg`,
+      `${URL_PATH}/farm-to-table-dinner.jpeg`,
+      `${URL_PATH}/chinese-food.jpeg`,
     ],
   },
   {
@@ -130,36 +140,28 @@ var DEFAULT_ITEMS = [
     name: "Kayak",
     price: "$215",
     location: "On the water",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-kayak.jpeg`, `${URL_PATH}/kayak.jpeg`],
   },
   {
     id: 16,
     name: "Ice Cream",
     price: "$12",
     location: "Within Walking Distance",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-ice-cream.jpeg`, `${URL_PATH}/icecream.jpeg`],
   },
   {
     id: 17,
     name: "REI Date",
     price: "$25",
     location: "Just down the road",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-rei.jpeg`, `${URL_PATH}/REI-date.jpeg`],
   },
   {
     id: 18,
     name: "Home Cooked Meal",
     price: "$55",
     location: "In Fridge",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-home-cooked-meal.jpeg`, `${URL_PATH}/bread.jpeg`],
   },
   {
     id: 19,
@@ -167,7 +169,13 @@ var DEFAULT_ITEMS = [
     price: "$5",
     location: "In Your Favorite Neighborhood",
     images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      `${URL_PATH}/mk-house.jpeg`,
+      `${URL_PATH}/house-tour-1.jpeg`,
+      `${URL_PATH}/house-tour-2.jpeg`,
+      `${URL_PATH}/house.jpeg`,
+      `${URL_PATH}/house2.jpeg`,
+      `${URL_PATH}/carpet.jpeg`,
+      `${URL_PATH}/sf-move.jpeg`,
     ],
   },
   {
@@ -175,27 +183,28 @@ var DEFAULT_ITEMS = [
     name: "Notarization",
     price: "$45",
     location: "UPS Store",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-notary.jpeg`, `${URL_PATH}/notary.jpeg`],
   },
   {
     id: 21,
     name: "Yoga Class",
     price: "$32",
     location: "On the beach",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-yoga.jpeg`, `${URL_PATH}/yoga-class.jpeg`],
   },
   {
     id: 22,
     name: "Sailing Trip",
     price: "$3200",
     location: "Between Islands",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-sailboat.jpeg`, `${URL_PATH}/bvi-trip.jpeg`],
+  },
+  {
+    id: 23,
+    name: "Couch",
+    price: "$650",
+    location: "The apartment across town",
+    images: [`${URL_PATH}/mk-couch.jpeg`, `${URL_PATH}/couch.jpeg`],
   },
 ];
 
@@ -205,9 +214,7 @@ var STEP1 = [
     name: "Non-alcoholic Beer",
     price: "Free!",
     location: "Somewhere in the house",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-beeer.jpeg`],
   },
 ];
 
@@ -217,9 +224,7 @@ var STEP2 = [
     name: "Pickleball with Family",
     price: "Free!",
     location: "Parent's House",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-pickleball.jpeg`, `${URL_PATH}/pickleball.jpeg`],
   },
 ];
 
@@ -229,9 +234,7 @@ var STEP3 = [
     name: "DoorDash",
     price: "Free!",
     location: "Somewhere outside the house",
-    images: [
-      "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
-    ],
+    images: [`${URL_PATH}/mk-doordash.jpeg`, `${URL_PATH}/doordash.jpeg`],
   },
 ];
 
