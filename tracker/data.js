b9b3758de79111c905bdcadb529fcde9f61d0146
Asa Schachar <asa.schachar+gh@gmail.com>
2023-10-15 22:41:38 -0700

    Add tracker

---
+++ tracker/data.js
@@ -0,0 +1,90 @@
+var STAGES = [
+  {
+    day: 0,
+    stage: "Zygote",
+  },
+  {
+    day: 1,
+    stage: "Two-cell Division",
+  },
+  {
+    day: 2,
+    stage: "Four-cell Division",
+  },
+  {
+    day: 3,
+    stage: "Eight-cell Division",
+  },
+  {
+    day: 4,
+    stage: "Blastocoel Formation",
+  },
+  {
+    day: 5,
+    stage: "Blastocyst",
+  },
+  {
+    day: 6,
+    stage: "Hatching",
+  },
+  {
+    day: 7,
+    stage: "Implantation",
+  },
+  {
+    day: 13,
+    stage: "Gastrulation: Embryonic Disc",
+  },
+  {
+    day: 14,
+    stage: "Gastrulation: Embryonic Disc - Primitive Streak",
+  },
+  {
+    day: 15,
+    stage: "Gastrulation: Formation of Definitive Endoderm",
+  },
+  {
+    day: 17,
+    stage: "Gastrulation: Formation of Definitive Mesoderm",
+  },
+  {
+    day: 18,
+    stage: "Neurulation: Neural Plate",
+  },
+  {
+    day: 19,
+    stage: "Neurulation: Neural Groove",
+  },
+  {
+    day: 20,
+    stage: "Neurulation: Neural Fold",
+  },
+  {
+    day: 21,
+    stage: "Neurulation: Neural Tube",
+  },
+  {
+    day: 22,
+    stage: "Neurulation: Neural Tube & Heart beating",
+  },
+  {
+    day: 23,
+    stage: "Neurulation: Neural Tube Closure",
+  },
+  {
+    day: 25,
+    stage: "Neurulation: Pericardial Bulge",
+  },
+  {
+    day: 27,
+    stage: "Neurulation: Developing Brain, Heart, Gut, and Neural Tissue",
+  },
+  {
+    day: 28,
+    stage: "Development: Developing Arm, Eye, and Ear",
+  },
+  {
+    day: 28,
+    stage: "Development: Developing Arm, Eye, and Ear",
+  },
+];
