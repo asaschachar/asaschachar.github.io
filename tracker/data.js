ad088386c46415c970889bbc3f66bbc9759f1ce0
Asa Schachar <asa.schachar+gh@gmail.com>
2023-10-18 17:53:27 -0700

    Push some more info

--- tracker/data.js
+++ tracker/data.js
@@ -65,7 +65,7 @@ var STAGES = [
   },
   {
     day: 22,
-    stage: "Neurulation: Neural Tube & Heart beating",
+    stage: "Neurulation: Neural Tube & Heart beating. Limb buds begin to form.",
   },
   {
     day: 23,
@@ -73,7 +73,8 @@ var STAGES = [
   },
   {
     day: 25,
-    stage: "Neurulation: Pericardial Bulge",
+    stage:
+      "Neurulation: Pericardial Bulge. Eye and ear structures are recognizable.",
   },
   {
     day: 27,
@@ -81,10 +82,63 @@ var STAGES = [
   },
   {
     day: 28,
-    stage: "Development: Developing Arm, Eye, and Ear",
+    stage: "Development: Developing Arm, Eye, Ear, Skin",
   },
   {
     day: 28,
     stage: "Development: Developing Arm, Eye, and Ear",
+  },
+  {
+    day: 31,
+    stage: "Development: Sprout arms, legs, hand, feet, fingers, and toes",
+  },
+  {
+    day: 42,
+    stage:
+      "Development: Face takes on more distinct features, and limb buds elongate.",
+  },
+  {
+    day: 45,
+    stage:
+      "Development: Intestines. Done with two kidney sets, third on the way. Sense of touch on lips and nose. Brain developing and able to wiggle limbs",
+  },
+  {
+    day: 56,
+    stage:
+      "Development: Most internal organs are formed, and sex organs begin to differentiate.",
+  },
+  {
+    day: 73,
+    stage: "Development: Sense of touch on genitals, palms, and soles of feet",
+  },
+  {
+    day: 84,
+    stage:
+      "Most major structures are present, and the fetus can swallow and produce urine.",
+  },
+  {
+    day: 98,
+    stage: "Development: Tooth buds appear in the gums.",
+  },
+  {
+    day: 105,
+    stage: "Development: Fetus can hear and respond to sound.",
+  },
+  {
+    day: 168,
+    stage:
+      "Development: Lungs continue to develop, and the fetus can make breathing movements.",
+  },
+  {
+    day: 196,
+    stage: "Development: Rapid eye movement (REM) sleep begins.",
+  },
+  {
+    day: 210,
+    stage: "Fetus can regulate its body temperature.",
+  },
+  {
+    day: 245,
+    stage: "Fetus typically turns head down in preparation for birth.",
   },
 ];
